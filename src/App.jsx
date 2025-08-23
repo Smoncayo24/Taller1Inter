f37a960156c3988ec0e56db53b84687d1906b639
smoncayo24 <smoncayo24@javerianacali.edu.co>
2025-08-22 23:16:27 -0500

    Taller

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Juan Pérez</h1>
+      <p>Diseñador UX en formación</p>
+    </div>
+  )
+}
+
+export default App
